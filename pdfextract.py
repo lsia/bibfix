8f1ea534a791d5f07eb09174122c643534c21734
Enrique Calot <ecalot@cloodie.com>
2015-01-16 13:10:41 -0300

    bugfix

--- pdfextract.py
+++ pdfextract.py
@@ -39,5 +39,5 @@ def getMetadata(file):
 #print processPdf(sys.argv[1],r"(eference|ibliogra|EFEREN|IBLIOGRA)")
 
 def getPapers():
-	return sorted([m.group(1) for m in [re.match(r"^([a-z0-9]*)\.pdf$",fn) for fn in os.listdir('pdf/')] if m])
+	return sorted([m.group(1) for m in [re.match(r"^([a-z0-9]*)\.pdf$",fn) for fn in os.listdir('library/')] if m])
 
